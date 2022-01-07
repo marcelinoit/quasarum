bd9b4a040a14b175d0642f1ea9c20441df9bb21c
marcelinoit <64853704+marcelinoit@users.noreply.github.com>
2022-01-07 21:54:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# quasarum
+ quasar
---
+++ src/pages/login.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="col-0 col-md-6 flex justify-center content-center">
+        <h5>Marcelino</h5>
+    </div>
+    <div class="col-0 col-md-6 flex justify-center content-center">
+      <img src="~assets/marcelino.jpg" style="width: 200px; height: 200px" class="responsive" alt="login-image">
+    </div>
+</template>
+
+<script>
+
+</script>
