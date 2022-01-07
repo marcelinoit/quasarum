31b2e2f6d1f8ce19ca146c091afc3824296eb2ad
marcelinoit <64853704+marcelinoit@users.noreply.github.com>
2022-01-07 22:01:00 +0200

    alteraçao do nome
    alteraçao do nome

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="col-0 col-md-6 flex justify-center content-center">
-        <h5>Marcelino</h5>
+        <h5>Marcelino Pedro Jr</h5>
     </div>
     <div class="col-0 col-md-6 flex justify-center content-center">
       <img src="~assets/marcelino.jpg" style="width: 200px; height: 200px" class="responsive" alt="login-image">
